d20f4a65115b833d934f18004c76f11e334f3a15
Daniel Zazzali <80864796+danielzazzali@users.noreply.github.com>
2024-09-05 09:09:39 -0400

    Update config_daughter_ip.sh

--- config_daughter_ip.sh
+++ config_daughter_ip.sh
@@ -1,2 +1,11 @@
-sudo nmcli c mod "Wired connection 1" ipv4.addresses 10.0.0.200/24 ipv4.method manual
-sudo nmcli con mod "Wired connection 1" ipv4.gateway 10.0.0.1
+#!/bin/bash
+
+sudo ifconfig
+
+sudo nmcli c mod "Wired connection 1" ipv4.addresses 172.16.23.202/24 ipv4.method manual
+sudo nmcli con mod "Wired connection 1" ipv4.gateway 172.16.23.1
+sudo nmcli con mod "Wired connection 1" ipv4.dns "146.83.129.4,8.8.8.8"
+
+sudo nmcli c down "Wired connection 1" && sudo nmcli c up "Wired connection 1"
+
+sudo ifconfig
