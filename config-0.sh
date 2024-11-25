4371f634db0b7a89021a4bf4abff42fdb480ee18
Daniel Zazzali <80864796+danielzazzali@users.noreply.github.com>
2024-11-25 14:12:18 -0300

    Update config-0.sh

--- config-0.sh
+++ config-0.sh
@@ -141,48 +141,6 @@ configure_sta_mode() {
     log_info "Station (STA) configuration completed successfully."
 }
 
-# Function to create nginx default and default81 files for STA mode
-create_nginx_files_sta() {
-    log_info "Creating nginx default and default81 files for STA..."
-
-    # default81 redirects to localhost:8000
-    echo -e "server {\n    listen 80;\n    server_name localhost;\n    location / {\n        proxy_pass http://localhost:8000/;\n    }\n}" | sudo tee /etc/nginx/sites-available/default81 > /dev/null
-
-    # default will remain empty for later configuration
-    echo -e "" | sudo tee /etc/nginx/sites-available/default > /dev/null
-}
-
-# Function to create systemd service to update nginx default with eth0 IP
-create_nginx_ip_update_service() {
-    log_info "Creating systemd service to update default with eth0 IP every 20 seconds..."
-
-    sudo tee /etc/systemd/system/update_nginx_ip.service > /dev/null <<EOF
-[Unit]
-Description=Update Nginx Proxy Pass to eth0 IP
-
-[Service]
-Type=simple
-ExecStart=/bin/bash -c 'IP=$(arp -n eth0 | grep -oP "(?<=\d+\.\d+\.\d+\.\d+)\s+\d+" | awk "{print \$1}") && sed -i "s|proxy_pass http://localhost:8000/;|proxy_pass http://\$IP:8000/;|" /etc/nginx/sites-available/default'
-Restart=always
-RestartSec=20s
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    # Enable and start the service
-    sudo systemctl enable update_nginx_ip.service
-    sudo systemctl start update_nginx_ip.service
-}
-
-# Function to create nginx default file for AP mode
-create_nginx_file_ap() {
-    log_info "Creating nginx default file for AP..."
-
-    echo -e "server {\n    listen 80;\n    server_name localhost;\n    location / {\n        proxy_pass http://localhost:8000/;\n    }\n}" | sudo tee /etc/nginx/sites-available/default > /dev/null
-}
-
-
 # Main function to orchestrate the configuration process
 main() {
     log_info "Starting installation script for Raspberry Pi."
@@ -205,11 +163,8 @@ main() {
     # Step 6: Based on the user's choice, configure the system for either AP or STA
     if [[ "$mode_choice" == "AP" ]]; then
         configure_ap_mode
-        create_nginx_file_ap
     elif [[ "$mode_choice" == "STA" ]]; then
         configure_sta_mode
-        create_nginx_files_sta
-        create_nginx_ip_update_service
     else
         log_error "Invalid mode selected. Exiting."
         exit 1
